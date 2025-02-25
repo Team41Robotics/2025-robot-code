56e7352b60a91dd12eb2b236f5137ad6fcbf6044
Mason <mason@digiorgiofamily.net>
2025-02-25 18:59:29 -0500

    algae and intake subsystems complete

--- src/main/java/frc/robot/subsystems/algae/AlgaeIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeIOSparkMax.java
@@ -6,13 +6,17 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class AlgaeIOSparkMax implements AlgaeIO{
         
-        DutyCycleEncoder pivotEncoder = new DutyCycleEncoder(1);
-        SparkMax algaeMotor = new SparkMax(0, null);
+        private final DutyCycleEncoder pivotEncoder;
+        private final CANSparkMax algaeMotor;
+        private final PIDController m_PID;
 
+        public AlgaeIOSparkMax {
+                pivotEncoder = new DutyCycleEncoder(1);
+                algaeMotor = new CANSparkMax(0, null);
+                m_PID = new PIDController(0,0,0)
+        }
         @Override 
         public void updateInputs(AlgaeIOInputs inputs){
-
-        }
-
-
+                
+        }       
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,3 +1,21 @@
 package frc.robot.subsystems.intake;
+import org.littletonrobotics.junction.AutoLog;
 
-public class IntakeIO {}
+public interface IntakeIO {
+
+    public default void setVelocity(double velocity){};
+
+    public default void setVoltage(double voltage){};   
+
+    public default void stopMotor() {};
+    
+    public default void updateInputs(IntakeIOInputs inputs){};
+
+    @AutoLog
+    public static class IntakeIOInputs(){
+        public double velocity = 0.0;
+        public double voltage = 0.0;
+        public boolean beamBreakIsActive = false;
+        public double[] current = {};
+    }
+}
